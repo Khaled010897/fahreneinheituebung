af19bc0005d76e52da970f83acc66ed5f53833ae
Khaled Daoud <ic21b019@technikum-wien.at>
2022-12-03 14:07:40 +0100

    test

---
+++ src/test/java/at/fhtw/fahreinheitUebung/FahreinheitUebungApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.fahreinheitUebung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FahreinheitUebungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
